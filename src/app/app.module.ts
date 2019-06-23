b4947a8426d4391cff47d89de6618be98ef772e0
karsten-coba <krathlev@gmail.com>
2019-06-23 09:11:37 +0200

    Cleanup

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,7 +3,7 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
-import { MatInputModule, MatDialogModule, MatButtonModule } from '@angular/material';
+import { MatInputModule, MatButtonModule } from '@angular/material';
 
 import { LoginDialogComponent } from './login-dialog/login-dialog.component';
 
